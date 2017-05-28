2be2347716762b180592f57b9b3d5565b8d0caa3
Hcaepllams <hcaepllams@HcaepllamsdeMacBook-Pro.local>
2017-05-29 00:00:39 +0800

    answer for ex3

---
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -0,0 +1,43 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+hx = sigmoid(X * all_theta');
+[M, p] = max(hx, [], 2);
+% p是index，M只是这个index的值，因此对于这个例子来说，如果只是用p = max(hx, [], 2);的话得到的p其实是最大的概率的那个概率值，而不是具体的index，因为这个index才是我们关心的，是0-9里面的哪个。
+
+
+
+
+% =========================================================================
+
+
+end
